a22eeeab2d04006a1aaebea7bf1d70ab60a52816
Ethan Colbert <emcolbert2@gmail.com>
2019-09-28 15:07:01 -0400

    Project is completed, just need to polish a bit.

--- src/edu/bellarmine/emc/Application.java
+++ src/edu/bellarmine/emc/Application.java
@@ -23,13 +23,14 @@ public class Application {
 		String string, substring;//stores the two strings we need to work with
 		Scanner input = new Scanner(System.in);//for input
 		boolean isSubstring = false, manual = true;//whether or not "substring" is a substring of "string"
+		TextReader reader;//a TextReader to grab strings from a file
 		
 		/* This loop insures that we get valid input for the choice of whether to
 		 * enter strings manually or via a file (if input is invalid, the user is prompted again). */
 		boolean gotValid = false;
 		do {
 			
-			System.out.print("Would you like to enter strings manually or from a file? (M or F): ");
+			System.out.print("Would you like to enter sequences manually or from a file? (M or F): ");
 			String mORf = input.nextLine().toLowerCase();
 			
 			if (mORf.charAt(0) == 'm') {
@@ -49,10 +50,14 @@ public class Application {
 			boolean end = false;
 			do {
 				
+				System.out.println();//gives us a blank line
+				
 				System.out.print("Enter the subsequence to search for: ");
 				substring = input.nextLine().toLowerCase();
 				System.out.print("Enter the string to search through: ");
 				string = input.nextLine().toLowerCase();
+				
+				System.out.println();//gives us a blank line
 				
 				/* This loop pushes all the characters of the substring to the stack
 				 * in REVERSE order */
@@ -80,14 +85,16 @@ public class Application {
 				}
 				
 				if (isSubstring) {
-					System.out.println(substring + " is a subsequence of " + string);
+					System.out.println(substring + " IS A SUBSEQUENCE of " + string);
 				}
 				else {
-					System.out.println(substring + " is NOT a subsequence of " + string);
+					System.out.println(substring + " IS NOT A SUBSEQUENCE of " + string);
 				}
 				
 				charStack.clear();
 				isSubstring = false;
+				
+				System.out.println();//gives us a blank line
 				
 				System.out.print("Would you like to enter more sequences? (Yes or No): ");
 				goAgain = input.nextLine();
@@ -100,12 +107,57 @@ public class Application {
 		
 		else {
 			
-			//code for file input goes here
-			System.out.println("We elected to input strings via file (stub message).");
+			System.out.print("Enter the name of the file: ");
+			String fileName = input.nextLine();
+			reader = new TextReader(fileName);
+			
+			System.out.println();//gives us a blank line
+			
+			for (int i = 0; i < reader.getNumberOfLines(); i++) {
+				
+				substring = reader.getSubstring(i);
+				string = reader.getString(i);
+				
+				/* This loop pushes all the characters of the substring to the stack
+				 * in REVERSE order */
+				for (int k = 1; k <= substring.length(); k++) {
+					charStack.push(substring.charAt((substring.length() - k)));
+				}
+				
+				for (int j = 0; j < string.length(); j++) {
+					
+					/* If we find the character at the top of the stack (the next character in the substring) in "string",
+					 * we pop it off the stack. */
+					if (string.charAt(j) == charStack.peek()) {
+						charStack.pop();
+					}
+					
+					/* If the stack is empty, it means the substring is indeed a subsequence of "string". */
+					if (charStack.isEmpty()) {
+						
+						isSubstring = true;
+						
+						j = string.length();//ends the loop
+						
+					}
+					
+				}
+				
+				if (isSubstring) {
+					System.out.println(substring + " IS A SUBSEQUENCE of " + string);
+				}
+				else {
+					System.out.println(substring + " IS NOT A SUBSEQUENCE of " + string);
+				}
+				
+				charStack.clear();
+				isSubstring = false;
+				
+			}
 			
 		}
 		
-		System.out.println("<END RUN>");
+		System.out.println("\n<END RUN>");
 		
 		input.close();
 	}// end main
---
+++ src/edu/bellarmine/emc/TextReader.java
@@ -0,0 +1,106 @@
+package edu.bellarmine.emc;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * This class executes the program.
+ * 
+ * @author Ethan Colbert
+ * @version 1.0
+ * Programming Project 1
+ * Fall 2019
+ */
+
+public class TextReader {
+	
+	private File file;//the file we need to read
+	private Scanner fileReader;//reads through the file and grabs strings
+	private ArrayList<String> strings, substrings;//ArrayLists to hold the contents of the file
+	int numberOfLines;//how many lines are in the file
+	
+	/**
+	 * Empty-argument constructor - just instantiates things
+	 */
+	public TextReader() {
+		
+		strings = new ArrayList<String>();
+		substrings = new ArrayList<String>();
+		numberOfLines = 0;
+		
+	}// end empty-argument constructor
+	
+	public TextReader(String fileName) {
+		
+		strings = new ArrayList<String>();
+		substrings = new ArrayList<String>();
+		numberOfLines = 0;
+		
+		startReader(fileName);
+		
+		fillArrayLists();
+		
+	}
+	
+	/**
+	 * Returns the string stored in the given index of "strings"
+	 * @param index - the index to grab from
+	 * @return the string at "index"
+	 */
+	public String getString(int index) {
+		return strings.get(index);
+	}
+	
+	
+	/**
+	 * Returns the string stored in the given index of "substrings"
+	 * @param index - the index to grab from
+	 * @return the substring at "index"
+	 */
+	public String getSubstring(int index) {
+		return substrings.get(index);
+	}
+	
+	/**
+	 * @return the numberOfLines
+	 */
+	public int getNumberOfLines() {
+		return numberOfLines;
+	}
+
+	/**
+	 * SUPPORT METHOD - sets up the scanner to read from the file passed in
+	 * @param file - the name of the file the scanner will read from
+	 */
+	private void startReader(String fileName) {
+		
+		file = new File(fileName);
+		
+		try {
+			fileReader = new Scanner(file);
+			fileReader.useDelimiter("(,|\\p{javaWhitespace})+");
+		}
+		catch (Exception e) {
+			System.out.println("Failed to read from file.\nFile may be corrupted.");
+		}
+		
+	}// end "startReader" support method
+	
+	/**
+	 * SUPPORT METHOD - fills the ArrayLists with the contents of the file
+	 */
+	private void fillArrayLists() {
+		
+		while(fileReader.hasNextLine()) {
+			
+			substrings.add(fileReader.next().toLowerCase());
+			strings.add(fileReader.next().toLowerCase());
+			
+			numberOfLines++;
+			
+		}
+		
+	}// end "fillArrayLists" support method
+	
+}// end "TextReader" class
